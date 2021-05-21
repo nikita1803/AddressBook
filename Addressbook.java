712491fb8ec4b94c34f1dc1f195e8d805b477d45
nikita1803 <rainikita595@gmail.com>
2021-05-21 13:58:22 +0530

    Added: Created a contact in address book

--- Addressbook.java
+++ Addressbook.java
@@ -1,9 +1,50 @@
 package addressbook;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Addressbook 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
-	System.out.println("Welcome in Address Book");
+        System.out.println("Welcome in Address Book");
+        List<String> PersonDetail = new ArrayList<String>();
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the First name : ");
+        String fname = input.nextLine();
+
+        System.out.print("Enter the Last name : ");
+        String lname = input.nextLine();
+
+        System.out.print("Enter the Address : ");
+        String address = input.nextLine();
+
+        System.out.print("Enter the City : ");
+        String city = input.nextLine();
+
+        System.out.print("Enter the state : ");
+        String state = input.nextLine();
+
+        System.out.print("Enter the Zip Code : ");
+        String zip = input.nextLine();
+
+        System.out.print("Enter the phone number: ");
+        String phone = input.nextLine();
+        
+        System.out.print("Enter the Email: ");
+        String email = input.nextLine();
+
+        PersonDetail.add(fname);
+        PersonDetail.add(lname);
+        PersonDetail.add(address);
+        PersonDetail.add(city);
+        PersonDetail.add(state);
+        PersonDetail.add(zip);
+        PersonDetail.add(phone);
+        PersonDetail.add(email);
+
 	}
 }
+
