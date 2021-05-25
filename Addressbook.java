5014387ab00126fb49b71350b2ba0726b9356866
nikita1803 <rainikita595@gmail.com>
2021-05-25 23:11:43 +0530

    Added:Add multiple person to address book

--- Addressbook.java
+++ Addressbook.java
@@ -18,9 +18,10 @@ public class Addressbook
 	 * create contact is a function in which we add the contact details
 	 * @return array list
 	 */
-	public static ArrayList<String> createContact()
+	boolean check=false;
+	public static ArrayList<ContactDetails> createContact()
 	{
-		ArrayList<String> PersonDetail = new ArrayList<String>(1);
+		ArrayList<ContactDetails> PersonDetail = new ArrayList<ContactDetails>();
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter the First name : ");
@@ -39,52 +40,98 @@ public class Addressbook
         String state = input.nextLine();
 
         System.out.print("Enter the Zip Code : ");
-        String zip = input.nextLine();
+        int zip = input.nextInt();
 
         System.out.print("Enter the phone number: ");
-        String phone = input.nextLine();
-        
+        int phone = input.nextInt();input.nextLine();
         System.out.print("Enter the Email: ");
         String email = input.nextLine();
-        
-		PersonDetail.add(fname);
-        PersonDetail.add(lname);
-        PersonDetail.add(address);
-        PersonDetail.add(city);
-        PersonDetail.add(state);
-        PersonDetail.add(zip);
-        PersonDetail.add(phone);
-        PersonDetail.add(email);
+        PersonDetail.add(new ContactDetails(fname, lname, address, city, state, zip, phone, email));
 
-        for (String str : PersonDetail)
+        for (ContactDetails str : PersonDetail)
         {
             System.out.println(str + " "); 
         }
         return PersonDetail;
 	}
+	public static boolean checkName(ArrayList<ContactDetails> personDetail) {
+		Scanner input = new Scanner(System.in);
+        System.out.println("Enter First Name");
+        String firstName = input.nextLine();
+        System.out.println("Enter Last Name");
+        String lastName = input.nextLine();
+
+        for (int indexNum = 0; indexNum < personDetail.size(); indexNum++) {
+            if (firstName.equals(personDetail.get(indexNum).getFirstName()) && lastName.equals(personDetail.get(indexNum).getLastName())) {
+                System.out.println("Contact Name Exists");
+                return true;
+            }
+        }
+        return false;
+    }
+	
+	
+	
+	
+	
+	
 	/**
 	 * created a function to edit the details from the console.
 	 * @param personDetail
 	 */
-	public static void editContact( ArrayList<String> personDetail)
+	
+
+	public static void editContact( ArrayList<ContactDetails> personDetail, boolean check)
 	{
-		ListIterator<String> iterator = personDetail.listIterator();
+		ListIterator<ContactDetails> iterator = personDetail.listIterator();
+		if(check)
+		{
 		System.out.println("enter old value");
 		Scanner input = new Scanner(System.in);
+		int i=0;
 		String originalValue = input.nextLine();
+		
 		System.out.println("enter new value");
 		String value = input.nextLine();
-		while (iterator.hasNext()) {
-		     String next = iterator.next();
-		     if (next.equals(originalValue)) {
-		         //Replace element
-		         iterator.set(value);
-		     } 
-		 }
-		 for (String str : personDetail)
+		
+		for(i=0;i<personDetail.size();i++)
+		{
+			
+			if(personDetail.get(i).getFirstName().equals(originalValue))
+			{
+			personDetail.get(i).setFirstName(value);
+			}
+			if(personDetail.get(i).getLastName().equals(originalValue))
+			{
+			personDetail.get(i).setLastName(value);
+			}
+			if(personDetail.get(i).getArea().equals(originalValue))
+			{
+			personDetail.get(i).setArea(value);
+			}
+			if(personDetail.get(i).getCity().equals(originalValue))
+			{
+			personDetail.get(i).setCity(value);
+			}
+			if(personDetail.get(i).getState().equals(originalValue))
+			{
+			personDetail.get(i).setState(value);
+			}
+			if(personDetail.get(i).getEmail().equals(originalValue))
+			{
+			personDetail.get(i).setEmail(value);
+			}
+			
+		}
+		 for (ContactDetails str : personDetail)
 	        {
 	            System.out.println(str + " "); 
 	        }
+		}
+		else
+		{
+			System.out.println("Contact not exist");
+		}
 	}
 	/**
 	 * delete contact is a function is use to delete the value 
@@ -92,7 +139,7 @@ public class Addressbook
 	 * iterator is use to iterate all the details person
 	 * hasNext keyword is use to iterate next value till the array length.
 	 */
-	public static void deleteContact(ArrayList<String> personDetail)
+	public static void deleteContact(ArrayList<ContactDetails> personDetail)
 	{
 		Iterator itr = personDetail.iterator();
 		System.out.println("enter value to delete");
@@ -104,7 +151,7 @@ public class Addressbook
             if (x.equals(deleteValue))
                 itr.remove();
         }
-        for (String str : personDetail)
+        for (ContactDetails str : personDetail)
         {
             System.out.println(str + " "); 
         }
@@ -116,19 +163,26 @@ public class Addressbook
 	public static void main(String[] args) 
 	{
         System.out.println("Welcome in Address Book");
-        ArrayList<String> personDetail= createContact();
+        ArrayList<ContactDetails> PersonDetail = new ArrayList<ContactDetails>();
+        ArrayList<ContactDetails> personDetail= createContact();
+        
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the First name : ");
-        String fname = input.nextLine();
+        String fname=input.nextLine();
         System.out.print("What you want to perform ? Press 1 for Add the details , press 2 for edit the details , press 3 for delete the details : ");
         int option = input.nextInt();
         switch(option)
         {
         	case 1:
         		createContact();
+        		for (ContactDetails str : personDetail)
+    	        {
+    	            System.out.println(str + " "); 
+    	        }
         	break;
         	case 2:
-        		editContact(personDetail);
+        		boolean check=checkName(personDetail);
+        		editContact(personDetail,check);
         	break;
         	case 3:
         		deleteContact(personDetail);
@@ -136,6 +190,102 @@ public class Addressbook
         	default:
         		System.out.print("Please enter the valid number : ");	
         } 
+       
 	}
+}
+class ContactDetails{
+
+    private String firstName;
+    private String lastName;
+    private String area;
+    private String city;
+    private String state;
+    private int zip;
+    private int phoneNumber;
+    private String email;
+
+    public ContactDetails(String firstName, String lastName, String area, String city, String state, int zip, int phoneNumber, String email) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.area = area;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getArea()
+    {
+    	return this.area;
+    }
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return this.state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+    public int getZip(){
+    
+    	return this.zip;
+    }
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+    
+    public int getPhoneNumber()
+    {
+    	return this.phoneNumber;
+    }
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getEmail()
+    {
+    	return this.email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String toString() {
+
+        return "Details of: " + firstName + " " + lastName + "\n"
+                + "Address: " + area + "\n"
+                + "City: " + city + "\n"
+                + "State: " + state + "\n"
+                + "Zip: " + zip + "\n"
+                + "Phone Number: " + phoneNumber + "\n"
+                + "Email: " + email;
+    }
 }
 
