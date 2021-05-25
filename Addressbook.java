4a938efa62140c8906093b592a01f6338f9cda46
nikita1803 <rainikita595@gmail.com>
2021-05-26 00:31:14 +0530

    Added: create a method to add multiple address book with unique name

--- Addressbook.java
+++ Addressbook.java
@@ -54,76 +54,75 @@ public class Addressbook
         }
         return PersonDetail;
 	}
-	public static boolean checkName(ArrayList<ContactDetails> personDetail) {
+	/**
+	 * check name is function which is use to check whether the user is exist or not.
+	 * @param personDetail
+	 * @return boolean value
+	 */
+	public static boolean checkName(ArrayList<ContactDetails> personDetail) 
+	{
 		Scanner input = new Scanner(System.in);
         System.out.println("Enter First Name");
         String firstName = input.nextLine();
         System.out.println("Enter Last Name");
         String lastName = input.nextLine();
 
-        for (int indexNum = 0; indexNum < personDetail.size(); indexNum++) {
-            if (firstName.equals(personDetail.get(indexNum).getFirstName()) && lastName.equals(personDetail.get(indexNum).getLastName())) {
+        for (int indexNum = 0; indexNum < personDetail.size(); indexNum++) 
+        {
+            if (firstName.equals(personDetail.get(indexNum).getFirstName()) && lastName.equals(personDetail.get(indexNum).getLastName())) 
+            {
                 System.out.println("Contact Name Exists");
                 return true;
             }
         }
         return false;
     }
-	
-	
-	
-	
-	
-	
 	/**
 	 * created a function to edit the details from the console.
-	 * @param personDetail
+	 * @param personDetail , check
+	 * first it check the value is equal to the original value if exist then you can edit the existing value. 
 	 */
-	
-
 	public static void editContact( ArrayList<ContactDetails> personDetail, boolean check)
 	{
-		ListIterator<ContactDetails> iterator = personDetail.listIterator();
 		if(check)
 		{
-		System.out.println("enter old value");
-		Scanner input = new Scanner(System.in);
-		int i=0;
-		String originalValue = input.nextLine();
-		
-		System.out.println("enter new value");
-		String value = input.nextLine();
-		
-		for(i=0;i<personDetail.size();i++)
-		{
+			System.out.println("enter old value");
+			Scanner input = new Scanner(System.in);
+			int i=0;
+			String originalValue = input.nextLine();
 			
-			if(personDetail.get(i).getFirstName().equals(originalValue))
+			System.out.println("enter new value");
+			String value = input.nextLine();
+		
+			for(i=0;i<personDetail.size();i++)
 			{
-			personDetail.get(i).setFirstName(value);
-			}
-			if(personDetail.get(i).getLastName().equals(originalValue))
-			{
-			personDetail.get(i).setLastName(value);
-			}
-			if(personDetail.get(i).getArea().equals(originalValue))
-			{
-			personDetail.get(i).setArea(value);
-			}
-			if(personDetail.get(i).getCity().equals(originalValue))
-			{
-			personDetail.get(i).setCity(value);
-			}
-			if(personDetail.get(i).getState().equals(originalValue))
-			{
-			personDetail.get(i).setState(value);
-			}
-			if(personDetail.get(i).getEmail().equals(originalValue))
-			{
-			personDetail.get(i).setEmail(value);
-			}
 			
-		}
-		 for (ContactDetails str : personDetail)
+				if(personDetail.get(i).getFirstName().equals(originalValue))
+				{
+				personDetail.get(i).setFirstName(value);
+				}
+				if(personDetail.get(i).getLastName().equals(originalValue))
+				{
+				personDetail.get(i).setLastName(value);
+				}
+				if(personDetail.get(i).getArea().equals(originalValue))
+				{
+				personDetail.get(i).setArea(value);
+				}
+				if(personDetail.get(i).getCity().equals(originalValue))
+				{
+				personDetail.get(i).setCity(value);
+				}
+				if(personDetail.get(i).getState().equals(originalValue))
+				{
+				personDetail.get(i).setState(value);
+				}
+				if(personDetail.get(i).getEmail().equals(originalValue))
+				{
+				personDetail.get(i).setEmail(value);
+				}
+			}
+			for (ContactDetails str : personDetail)
 	        {
 	            System.out.println(str + " "); 
 	        }
@@ -165,7 +164,8 @@ public class Addressbook
         System.out.println("Welcome in Address Book");
         ArrayList<ContactDetails> PersonDetail = new ArrayList<ContactDetails>();
         ArrayList<ContactDetails> personDetail= createContact();
-        
+        MultipleAddressBook.addAdressBookDetails();
+        MultipleAddressBook.addAdressBookDetails();
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the First name : ");
         String fname=input.nextLine();
@@ -189,12 +189,15 @@ public class Addressbook
         	break;
         	default:
         		System.out.print("Please enter the valid number : ");	
-        } 
-       
+        }  
 	}
 }
-class ContactDetails{
-
+/**
+ * contact details is a class to store the details of contact
+ * @author OM NAMO NAMAH
+ */
+class ContactDetails
+{
     private String firstName;
     private String lastName;
     private String area;
@@ -203,9 +206,14 @@ class ContactDetails{
     private int zip;
     private int phoneNumber;
     private String email;
+    
+    /**
+     * here is a parameterized constructor of the the class
+     * having parameters are the variable declare in the class of private type. 
+     */
 
-    public ContactDetails(String firstName, String lastName, String area, String city, String state, int zip, int phoneNumber, String email) {
-
+    public ContactDetails(String firstName, String lastName, String area, String city, String state, int zip, int phoneNumber, String email) 
+    {
         this.firstName = firstName;
         this.lastName = lastName;
         this.area = area;
@@ -215,70 +223,72 @@ class ContactDetails{
         this.phoneNumber = phoneNumber;
         this.email = email;
     }
-
-    public String getFirstName() {
+    public String getFirstName() 
+    {
         return this.firstName;
     }
-
-    public void setFirstName(String firstName) {
+    public void setFirstName(String firstName) 
+    {
         this.firstName = firstName;
     }
-
-    public String getLastName() {
+    public String getLastName() 
+    {
         return this.lastName;
     }
-
-    public void setLastName(String lastName) {
+    public void setLastName(String lastName) 
+    {
         this.lastName = lastName;
     }
     public String getArea()
     {
     	return this.area;
     }
-    public void setArea(String area) {
+    public void setArea(String area) 
+    {
         this.area = area;
     }
-
-    public String getCity() {
+    public String getCity() 
+    {
         return this.city;
     }
-
-    public void setCity(String city) {
+    public void setCity(String city) 
+    {
         this.city = city;
     }
-
-    public String getState() {
+    public String getState() 
+    {
         return this.state;
     }
-
-    public void setState(String state) {
+    public void setState(String state) 
+    {
         this.state = state;
     }
-    public int getZip(){
-    
+    public int getZip()
+    {
     	return this.zip;
     }
-    public void setZip(int zip) {
+    public void setZip(int zip) 
+    {
         this.zip = zip;
     }
-    
     public int getPhoneNumber()
     {
     	return this.phoneNumber;
     }
-    public void setPhoneNumber(int phoneNumber) {
+    public void setPhoneNumber(int phoneNumber) 
+    {
         this.phoneNumber = phoneNumber;
     }
     public String getEmail()
     {
     	return this.email;
     }
-    public void setEmail(String email) {
+    public void setEmail(String email) 
+    {
         this.email = email;
     }
-
-    public String toString() {
-
+    public String toString() 
+    {
         return "Details of: " + firstName + " " + lastName + "\n"
                 + "Address: " + area + "\n"
                 + "City: " + city + "\n"
@@ -287,5 +297,39 @@ class ContactDetails{
                 + "Phone Number: " + phoneNumber + "\n"
                 + "Email: " + email;
     }
-}
+  }
+/**
+ * Multiple address book is a class which is use to store the multiple address book with unique name.
+ */
+class MultipleAddressBook
+{
+	public static int indexNum;
+	public static ArrayList<ContactDetails> list = new ArrayList<>();
+	public String addressBookName;
+	public String firstName;
+	public String lastName;
+	public String area, city, state, email;
+	public int zip, phoneNumber;
 
+	/**
+	 * parameterized constructor of the class
+	 * @param addressBookName
+	 */
+	public MultipleAddressBook(String addressBookName) 
+	{
+		this.addressBookName = addressBookName;
+	}
+	public static Scanner sc = new Scanner(System.in);
+	public static List<String> allDetails = new ArrayList<>();
+	public static ArrayList<MultipleAddressBook > addressBook = new ArrayList<>();
+	private static int bookNumber = 0;
+	/**
+ 	*this is function which is use to add the address book . 
+ 	*/
+	public static void addAdressBookDetails() 
+	{
+		System.out.println("Enter Name of Address Book");
+		String name = sc.next();
+		addressBook.add(new MultipleAddressBook(name));
+	}
+}
